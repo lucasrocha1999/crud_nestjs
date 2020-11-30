0f9f130852688ff1bb069311f35a2ee7bd8e5a05
Lucas Lira <lucasrocha446@gmail.com>
2020-11-29 23:59:33 -0300

    feat: add method create

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { TaskService } from './shared/task.service';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Task } from './shared/task';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+  constructor(
+    private taskService: TaskService
+  ) { }
+
+  @Get()
+  async getAll() : Promise<Task[]> {
+    return this.taskService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number) : Promise<Task> {
+    return this.taskService.getById(id);
+  }
+}
