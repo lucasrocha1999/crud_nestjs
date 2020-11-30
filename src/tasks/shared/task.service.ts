5aaf3209908a29e73017b2e6f41af626b13fcd5e
Lucas Lira <lucasrocha446@gmail.com>
2020-11-30 00:00:14 -0300

    feat: add method update

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -26,11 +26,25 @@ export class TaskService {
   }
 
   create(task: Task) {
+    let lastId = 0;
+    if (this.tasks.length > 0) {
+      lastId = this.tasks[this.tasks.length - 1].id;
+    }
 
+    task.id = lastId + 1;
+    this.tasks.push(task);
+
+    return task;
   }
 
   update(task: Task) {
+    const taskArray = this.getById(task.id);
+    if (taskArray) {
+      taskArray.description = task.description;
+      taskArray.completed = task.completed;
+    }
 
+    return taskArray;
   }
 
   delete(id: number) {
