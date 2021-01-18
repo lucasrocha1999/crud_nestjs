d1c4cbbacdb04f17339f69c32a2b6746554f8ad3
Lucas Lira <lucasrocha446@gmail.com>
2021-01-18 16:36:12 -0300

    feat: blocking useguards routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { User } from './shared/user';
 import { UsersService } from './shared/users.service';
-import { Controller, Get, Param, Body, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Put, Delete, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/shared/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -8,26 +9,31 @@ export class UsersController {
     private usersService: UsersService,
   ) { }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAll(): Promise<User[]> {
     return this.usersService.getAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async getById(@Param('id') id: string): Promise<User> {
     return this.usersService.getById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() user: User): Promise<User> {
     return this.usersService.create(user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async update(@Param('id') id: string, @Body() user: User): Promise<User> {
     return this.usersService.update(id, user);
   }
   
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     this.usersService.delete(id);
